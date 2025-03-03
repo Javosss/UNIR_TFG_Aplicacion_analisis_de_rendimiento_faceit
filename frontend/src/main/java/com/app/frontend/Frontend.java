2f5bd57643d0cc6bccc37dd9f7009935046dfd96
Javosss <itzjavos@gmail.com>
2025-03-03 18:02:47 +0100

    Configuración del Frontend para la aplicación de escritorio

---
+++ frontend/src/main/java/com/app/frontend/Frontend.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.app.frontend;
+
+/**
+ *
+ * @author Javier
+ */
+public class Frontend {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
